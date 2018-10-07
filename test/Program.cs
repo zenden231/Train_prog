dab154b3d38fb89e60b2b243d16fefc888bc8f05
zenden231 <zenden231@gmail.com>
2018-10-08 00:53:40 +0200

    Add dictionary generate to ReadExcel

--- test/Program.cs
+++ test/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.OleDb;
-using System.Text;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ReadExcel
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Excel.Application ObjWorkExcel = new Excel.Application(); //открыть эксель
             Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@"C:\Users\Uzzer\Desktop\test.xlsx"); //открыть файл
@@ -19,17 +16,27 @@ namespace ReadExcel
             int lastColumn = (int)lastCell.Column;//!сохраним непосредственно требующееся в дальнейшем
             int lastRow = (int)lastCell.Row;
             //-------------------------------------
+            Dictionary<string, string> obj = new Dictionary<string, string>();
             string[,] list = new string[lastCell.Column, lastCell.Row]; // массив значений с листа равен по размеру листу
             for (int i = 0; i < (int)lastCell.Column; i++) //по всем колонкам
                 for (int j = 0; j < (int)lastCell.Row; j++) // по всем строкам
                     list[i, j] = ObjWorkSheet.Cells[j + 1, i + 1].Text.ToString();//считываем текст в строку
+            for (int n = 0; n < list.GetLength(1); n++)
+            {
+                if (list[0, n].ToString() == "" || list[1, n].ToString() == "")
+                {
+                    continue;
+                }
+                obj.Add(list[0,n].ToString(), list[1, n].ToString());
+            }
+            foreach (KeyValuePair<string, string> keyValue in obj)
+            {
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+            }
             ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
             ObjWorkExcel.Quit(); // выйти из экселя
             GC.Collect(); // убрать за собой -- в том числе не используемые явно объекты !
-            for (int i = 0; i < lastColumn; i++) //по всем колонкам
-                for (int j = 0; j < lastRow; j++) // по всем строкам 
-                    Console.Write(list[i, j]);//выводим строку
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
